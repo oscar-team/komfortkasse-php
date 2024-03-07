7a944c2160f4a97a8bf8a6d274d80452bc29f208
Mueez Sattar Khan <msk@hejoscar.dk>
2024-03-07 16:11:43 +0100

    Fix submit order api body

--- src/Requests/SubmitOrder.php
+++ src/Requests/SubmitOrder.php
@@ -4,9 +4,12 @@ namespace OscarTeam\KomfortkassePhp\Requests;
 
 use Saloon\Enums\Method;
 use Saloon\Http\Request;
+use Saloon\Traits\Body\HasJsonBody;
 
 class SubmitOrder extends Request
 {
+    use HasJsonBody;
+
     protected Method $method = Method::POST;
 
     public function __construct(public readonly string $orderNumber, public readonly array $orderData)
